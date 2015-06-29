382e8c4c0854581b8c50d28cdb078d5249936eb8
liuzhi_1989 <liuzhi_1989@gameboxcloud.com>
2015-06-29 16:23:33 +0800

    modify JobService

--- src/packages/job/JobService.lua
+++ src/packages/job/JobService.lua
@@ -23,93 +23,64 @@ THE SOFTWARE.
 ]]
 
 local pairs = pairs
-local tonumber = tonumber
 local json_encode = json.encode
 local json_decode = json.decode
-local os_time = os.time
 local string_format = string.format
-
-local _JOB_KEY = "_JOB_KEY"
-local _JOB_HASH = "_JOB_HASH"
-
-local _JOB_PRIORITY_NORMAL = 5000
 
 local JobService = class("JobService")
 
-function JobService:ctor(redis, beans, config)
-    if not redis or not beans then
-        throw("job service is initialized failed: redis or beans is invalid.")
+JobService.DEFAULT_PRIORITY = 5000
+JobService.DEFAULT_TTR      = 120
+
+function JobService:ctor(beans, config)
+    if not beans then
+        throw("job service is initialized failed: beans is invalid.")
     end
     if not config then
-        throw("job service is initialized failed: can't get config.")
+        throw("job service is initialized failed: config is invalid.")
     end
-    self._redis = redis
     self._beans = beans
+
     self._tube = string.format("job-%d", config.appIndex)
+    self._beans:command("use", self._tube)
 end
 
 function JobService:add(action, data, delay, priority, ttr)
-    local beans = self._beans
-    local redis = self._redis
-
-    delay = delay or 0
-    priority = priority or _JOB_PRIORITY_NORMAL
-    ttr = ttr or 120
-
-    if not action then
-        throw("job service add job failed: job action is null.")
+    if type(action) ~= "string" then
+        throw("add job failed, invalid action name")
     end
 
-    -- jobId means a redis id of this job.
-    local jobId, err = redis:command("INCR", _JOB_KEY)
-    if not jobId then
-        return nil, string_format("job service generate job id failed: %s", err)
+    if type(delay) ~= "number" then
+        delay = 0
+    end
+    if type(priority) ~= "number" then
+        priority = JobService.DEFAULT_PRIORITY
+    end
+    if type(ttr) ~= "number" then
+        ttr = JobService.DEFAULT_TTR
     end
 
-    local job = {}
-    job.id = jobId
-    job.joined_time = os_time()
-    job.action = action
-    job.arg = data
-    job.delay = delay
-    job.priority = priority
-    job.ttr = ttr
+    local job = {
+        action   = action,
+        data     = data,
+        delay    = delay,
+        priority = priority,
+        ttr      = ttr,
+    }
 
-    -- put job to beanstalkd
-    beans:command("use", self._tube)
-
-    -- jobBid means job id in beanstalkd.
-    jobBid = beans:command("put", json_encode(job), tonumber(priority), tonumber(delay), tonumber(ttr))
-    printInfo("job Bid = %s", jobBid)
-
-    -- store job info to redis for persistence
-    job.bid = jobBid
-    local ok, err = redis:command("HSET", _JOB_HASH, jobId, json_encode(job))
-    if not ok then
-        throw("job service newJob() store job into redis failed: %s", err)
+    local id, line = self._beans:command("put", json_encode(job), priority, delay, ttr)
+    if not id then
+        throw(string_format("add job failed, %s", line))
     end
 
-    return jobId
+    job.id = id
+    job.line = line
+    return job
 end
 
 function JobService:query(jobId)
-    local redis = self._redis
-
-    local job, err = redis:command("HGET", _JOB_HASH, jobId)
-    if not job then
-        return nil, string_format("job service query failed: %s", err)
-    end
-
-    if ngx and job == ngx.null then
-        return nil, string_format("job service query failed: job[%d] does not exist.", tonumber(jobId))
-    end
-
-    job, err = json_decode(job)
-    if not job then
-        return nil, string_format("job service query, the contents of job[%d] is invalid: %s", tonumber(jobId), err)
-    end
-
-    return job, nil
+    local id, line = self._beans:command("peek", jobId)
+    
 end
 
 function JobService:remove(jobId)
