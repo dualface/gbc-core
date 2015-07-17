b3b8bbdc5a40d7eabefd6aeaae504e4c787fadb6
liuzhi_1989 <liuzhi_1989@gameboxcloud.com>
2015-07-17 15:13:52 +0800

    fix install_dev.sh

--- install_dev.sh
+++ install_dev.sh
@@ -1,35 +1,37 @@
 #!/bin/bash
 
 CUR_DIR=$(cd "$(dirname $0)" && pwd)
-PARENT_DIR=`dirname "$CUR_DIR"`
-DEST_DIR=$PARENT_DIR/instance
+PARENT_DIR=$(dirname "$CUR_DIR")
+DEST_DIR="$PARENT_DIR/instance"
 
-sudo $CUR_DIR/install.sh --prefix=$DEST_DIR
+sudo "$CUR_DIR/install.sh" --prefix="$DEST_DIR"
 
-cd $DEST_DIR
+cd "$DEST_DIR"
 
 sudo chown -R $USER .
 
 rm -fr apps
-ln -s ../core/apps apps
+ln -s "$CUR_DIR/apps" apps
 
 rm -fr conf
-ln -s ../core/conf conf
+ln -s "$CUR_DIR/conf" conf
 
 rm -fr src
-ln -s ../core/src src
+ln -s "$CUR_DIR/src" src
+
+rm -fr tests
+ln -s "$CUR_DIR/tests" tests
 
 rm start_server
-ln -s ../core/shells/start_server start_server
+ln -s "$CUR_DIR/shells/start_server" start_server
 
 rm stop_server
-ln -s ../core/shells/stop_server stop_server
+ln -s "$CUR_DIR/shells/stop_server" stop_server
 
 rm check_server
-ln -s ../core/shells/check_server check_server
+ln -s "$CUR_DIR/shells/check_server" check_server
 
 rm init.inc
 rm init.lua
-ln -s ../core/shells/init.inc init.inc
-ln -s ../core/shells/init.lua init.lua
-
+ln -s "$CUR_DIR/shells/init.inc" init.inc
+ln -s "$CUR_DIR/shells/init.lua" init.lua
