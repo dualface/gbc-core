f76ded860b26b891f3a083b0c5b6bb9fc2739eac
bazhi <bazhicc@163.com>
2015-12-10 21:46:34 +0800

    增加清除操作

--- src/packages/bService/ConnectIDService.lua
+++ src/packages/bService/ConnectIDService.lua
@@ -1,7 +1,8 @@
 local ConnectIDService = cc.class("ConnectIDService")
 
-local CONNECTS_ID_DICT = "_CON_ID_DICT"
-local CONNECTS_TAG_DICT = "_CON_TAG_DICT"
+local CONNECTS_ID_DICT = "CON_ID_DICT"
+local CONNECTS_TAG_DICT = "CON_TAG_DICT"
+local CONNECTS = "CONNECTS"
 
 function ConnectIDService:ctor(redis)
     self._Redis = redis
@@ -9,6 +10,19 @@ end
 
 function ConnectIDService:getConnectId(tag)
     return self._Redis:hget(CONNECTS_TAG_DICT, tag)
+end
+
+function ConnectIDService:getConnects()
+    return self._Redis:smembers(CONNECTS)
+end
+
+function ConnectIDService:clearConnects()
+    local redis = self._Redis
+    redis:initPipeline()
+    redis:del(CONNECTS_ID_DICT)
+    redis:del(CONNECTS_TAG_DICT)
+    redis:del(CONNECTS)
+    redis:commitPipeline()
 end
 
 function ConnectIDService:getTag(connectid)
@@ -23,6 +37,7 @@ function ConnectIDService:save(connectid, tag)
     redis:initPipeline()
     redis:hset(CONNECTS_ID_DICT, connectid, tag)
     redis:hset(CONNECTS_TAG_DICT, tag, connectid)
+    redis:sadd(CONNECTS, connectid)
     redis:commitPipeline()
 end
 
@@ -34,6 +49,7 @@ function ConnectIDService:remove(connectid, tag)
     redis:initPipeline()
     redis:hdel(CONNECTS_ID_DICT, connectid)
     redis:hdel(CONNECTS_TAG_DICT, tag)
+    redis:srem(CONNECTS, connectid)
     redis:commitPipeline()
 end
 
