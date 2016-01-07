ddaaedb99d86c65c9b8665abbe81f3cdf1df7958
bazhi <bazhicc@163.com>
2016-01-07 14:11:24 +0800

    support mission

---
+++ src/packages/bService/MissionService.lua
@@ -0,0 +1,70 @@
+local MissionService = cc.class("MissionService")
+
+function MissionService:ctor(connect, key)
+    self._Redis = connect:getRedis()
+    self._Key = key
+end
+
+function MissionService:getKey()
+    return "Mission:"..self._Key
+end
+
+function MissionService:addMission(Missionid)
+    self._Redis:zadd(self:getKey(), 0, Missionid)
+end
+
+function MissionService:removeMission(Missionid)
+    self._Redis:zrem(self:getKey(), Missionid)
+end
+
+function MissionService:finishMission(Missionid)
+    self._Redis:zadd(self:getKey(), -1, Missionid)
+end
+
+function MissionService:processMission(Missionid, times)
+    return self._Redis:zincrby(self:getKey(), times, Missionid)
+end
+
+function MissionService:clearMissions()
+    self._Redis:del(self:getKey())
+end
+
+function MissionService:getTimes(id)
+    return tonumber(self._Redis:zscore(self:getKey(), id)) or 0
+end
+
+function MissionService:getAll(noFinish)
+    local list
+    if noFinish then
+        list = self._Redis:zrangebyscore(self:getKey(), 0, "+inf", "WITHSCORES")
+    else
+        list = self._Redis:zrange(self:getKey(), 0, -1, "WITHSCORES")
+    end
+
+    local len = #list
+    local ret = {}
+    for i = 1, len, 2 do
+        table.insert(ret, {id = tonumber(list[i]) or 0, times = tonumber(list[i+1]) or 0})
+    end
+    return ret
+end
+
+function MissionService:getAllHashMap(noFinish)
+    local list
+    if noFinish then
+        list = self._Redis:zrangebyscore(self:getKey(), 0, "+inf", "WITHSCORES")
+    else
+        list = self._Redis:zrange(self:getKey(), 0, -1, "WITHSCORES")
+    end
+    local len = #list
+    local ret = {}
+    for i = 1, len, 2 do
+        local id = tonumber(list[i])
+        if id then
+            ret[id] = tonumber(list[i+1]) or 0
+        end
+    end
+    return ret
+end
+
+return MissionService
--- src/packages/bService/bService.lua
+++ src/packages/bService/bService.lua
@@ -1,10 +1,11 @@
 local _M = {
-    CacheService  = cc.import(".CacheService"),
-    FightService  = cc.import(".FightService"),
-    FriendService = cc.import(".FriendService"),
-    RankService   = cc.import(".RankService"),
-    RoomService   = cc.import(".RoomService"),
-    ShopService   = cc.import(".ShopService"),
+    CacheService    = cc.import(".CacheService"),
+    FightService    = cc.import(".FightService"),
+    FriendService   = cc.import(".FriendService"),
+    RankService     = cc.import(".RankService"),
+    RoomService     = cc.import(".RoomService"),
+    ShopService     = cc.import(".ShopService"),
+    MissionService  = cc.import(".MissionService"),
 
     JobService    = cc.import(".JobService"),
 
