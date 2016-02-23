5c467a001944f28be2b17be3b264bcdb40d09cf1
koala <koala@koala.local>
2016-02-23 13:20:57 +0800

    remove vagrant-triggers depends

--- Vagrantfile
+++ Vagrantfile
@@ -64,7 +64,7 @@ Vagrant.configure(2) do |config|
   # Enable provisioning with a shell script. Additional provisioners such as
   # Puppet, Chef, Ansible, Salt, and Docker are also available. Please see the
   # documentation for more information about their specific syntax and use.
-  config.vm.provision :shell, path: "vagrant-support/bootstrap.sh"
+  config.vm.provision :shell, run: "always", path: "vagrant-support/bootstrap.sh"
 
   # config.vm.provision "shell", inline: <<-SHELL
   #   sudo apt-get update
--- vagrant-support/bootstrap.sh
+++ vagrant-support/bootstrap.sh
@@ -3,26 +3,40 @@
 PROJECT_ROOT_DIR=/vagrant
 BOOTSTRAP_DIR=/vagrant/vagrant-bootstrap
 
-# setup packages
+function setup()
+{
+    # setup packages
+    cd /etc/apt/
+    cp sources.list sources.list.origin
+    cat sources.list.origin | sed s/archive.ubuntu.com/mirrors.aliyun.com/ > sources.list
 
-cd /etc/apt/
-cp sources.list sources.list.origin
-cat sources.list.origin | sed s/archive.ubuntu.com/mirrors.aliyun.com/ > sources.list
+    debconf-set-selections <<< 'mysql-server mysql-server/root_password password gamebox'
+    debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password gamebox'
 
-debconf-set-selections <<< 'mysql-server mysql-server/root_password password gamebox'
-debconf-set-selections <<< 'mysql-server mysql-server/root_password_again password gamebox'
+    apt-get update -y
+    apt-get upgrade -y
+    apt-get install -y mysql-server mysql-client
 
-apt-get update -y
-apt-get upgrade -y
-apt-get install -y mysql-server mysql-client
+    cd /vagrant/
+    sudo ./install.sh
 
-cd /vagrant/
-sudo ./install.sh
+    echo "INSTALL COMPLETED."
+    echo ""
+    echo ""
+}
+
+if [ ! -f /opt/gbc-core/start_server ]; then
+    setup
+fi
 
 # done
-
-
 /opt/gbc-core/start_server --debug
+echo ""
+echo "waiting 5 seconds..."
+sleep 5
+echo ""
+echo ""
+/opt/gbc-core/check_server
 
 echo ""
 echo ""
